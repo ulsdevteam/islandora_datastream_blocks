1796d69695c118d50a124e9937e101e67a8fc1f2
root <root@islandora>
2016-05-31 20:33:49 +0000

    initial commit

---
+++ templates/datastream_block.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Main view template.
+ *
+ * Variables available:
+ */
+?>
+<?php if ($datastream_contents): ?>
+<div class="<?php print $classes; ?>">
+  <?php print $datastream_contents; ?>
+</div> 
+<?php endif; ?>
